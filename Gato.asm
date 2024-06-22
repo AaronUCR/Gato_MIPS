78e25583fd00d0eda334f2f9b12ee2e0da96ff37
Aaron040 <129924091+Aaron040@users.noreply.github.com>
2024-06-21 20:04:21 -0600

    Comenzando el ciclo de los jugadores, se anadieron las funcioneas para que se registren las jugadas de ambos jugadores

--- Gato.asm
+++ Gato.asm
@@ -1,15 +1,70 @@
 .data
 	tablero: .word 0, 0, 0, 0, 0, 0, 0, 0, 0
 	mensajeInicial: .asciiz "Bienvenido al juego GATO, ganas haciendo una linea vertical, horizontal o diagonal.\n"
-	
+	mensajeJugador1: .asciiz "Turno del jugador 1 (X), realice el movimiento...\n"
+	mensajeJugador2: .asciiz "Turno del jugador 2 (l), realice el movimiento...\n"
+	jugadaInvalida: .asciiz "Jugada invalida, intente de nuevo...\n"
+	ganadorJugador1: .asciiz "Jugador 1 (X) ha ganado...\n"
+	ganadorJugador2: .asciiz "Jugador 2 (l) ha ganado...\n"
 .text 
-main:
+Comienzo:
+	la $s0, tablero
+	li $s1, 1
 	li $v0, 4
 	la $a0, mensajeInicial
 	syscall
 	jal imprimir_tablero
 	
+# Llamada a las funciones para que el jugador 1 realice el movimiento o gane (X)
+jugador_1:
+	# Mensaje para que el jugador 1 realice un movimiento
+	li $v0, 4
+	la $a0, mensajeJugador1
+	syscall
 	
+	# Se ingresa la posicion donde el jugador quiere jugar
+	li $v0, 5
+	syscall
+	move $a0, $v0
+	move $s2, $v0
+	
+	#Se verifica que la jugada sea valida
+	jal verificar_jugada
+	beq $v0, 1, jugada_validada1
+		li $v0, 4
+		la $a0, jugadaInvalida
+		syscall
+		j jugador_1
+	jugada_validada1:
+	jal jugada_jugador1
+	
+# Llamada a las funciones correspondientes de jugador 2 (l)
+jugador_2:
+	# Mensaje para que el jugador 1 realice un movimiento
+	li $v0, 4
+	la $a0, mensajeJugador2
+	syscall
+	
+	# Se ingresa la posicion donde el jugador quiere jugar
+	li $v0, 5
+	syscall
+	move $a0, $v0
+	move $s2, $v0
+	
+	#Se verifica que la jugada sea valida
+	jal verificar_jugada
+	beq $v0, 1, jugada_validada2
+		li $v0, 4
+		la $a0, jugadaInvalida
+		syscall
+		j jugador_2
+	jugada_validada2:
+	jal jugada_jugador2
+Fin:
+	li $v0, 10
+	syscall
+
+# Funcion que imprime el tablero en bitmap
 imprimir_tablero:
 # Dibujando linea horizontal 1
 	# Cargando el color del tablero (rojo)
@@ -89,22 +144,48 @@ imprimir_tablero:
 		Fin_linVert_2:
 jr $ra
 
-checar_jugada_valida:
+# Funcion que ejecuta la jugada para el jugador 1 y la guarda dentro de tablero
+jugada_jugador1:
+	addi $s1, $s1, 1
+	la $t0, tablero
+	li $t1, 1
+	li $t2, 4
+	move $t3, $s2
+	sub $t3, $t3, 1
+	mult $t3, $t2
+	mflo $t3
+	add $t0, $t0, $t3
+	sw $t1, 0($t0)
+jr $ra
+	
+# Funcion que ejecuta la jugada para el jugador 2 y la guarda dentro de tablero
+jugada_jugador2:
+	addi $s1, $s1, 1
+	la $t0, tablero
+	li $t1, 2
+	li $t2, 4
+	move $t3, $s2
+	sub $t3, $t3, 1
+	mult $t3, $t2
+	mflo $t3
+	add $t0, $t0, $t3
+	sw $t1, 0($t0)
+jr $ra
+
+# Funcion que verifica si las jugadas son correctas
+verificar_jugada:
 	# Checa si el número es >= 1
 	move $t0, $a0
 	bge $t0, 1, validez_1
-	
 	j jugada_invalida
 	
-
 	# Checa si el número es <= 9	
 validez_1:
 	ble $t0, 9, validez_2
 	j jugada_invalida
 	
-	
 validez_2:
-	# Checa si el campo ya fue llenado
+	# Checa si el campo ya fue llenado (Arreglar)
 	subu $t1, $t0, 1
 	li $t2, 4 
 	mult $t1, $t2
@@ -112,15 +193,13 @@ validez_2:
 	la $t4, tablero
 	add $t4, $t4, $t3
 	lw $t5, 0($t4) 
-		beq $t5, $0, jugada_valida 
-	
+	beq $t5, $0, jugada_valida 
 	
 jugada_invalida:
 	li $v0, 0
-	jr $ra
-	
+jr $ra
 	
 jugada_valida:
 	li $v0, 1
-	jr $ra
+jr $ra
 
